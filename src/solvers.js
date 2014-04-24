56a3c3c2ed089cc846313f01ccd3cc72758f7c79
jgpettibone <jeanette.pettibone@gmail.com>
2014-04-23 19:09:43 -0700

    Implemented countRookSolutions - not optimized and not particularly speedy

--- src/solvers.js
+++ src/solvers.js
@@ -15,39 +15,36 @@
 window.findNRooksSolution = function(n){
   var solution;
   var board = new Board({n:n});
-  console.log("board", board);
 
   // recursive subroutine - with everything you keep track of passed as arguments
-  var getRookSolution = function(rookBoard, startRow, numRows) {
-
-    // console.log(rookBoard);
+  var getRookSolution = function(rookBoard, rowIndex, numCols) {
 
     // exit strategy - when we're in the last row and we've placed a rook
     // want to return the rows since that's what's expected output
-    if (startRow === numRows) {
+    if (rowIndex === numCols) {
       return rookBoard.rows();
     }
 
     // otherwise keep trying to place the rook
-    for (var i = 0; i < numRows; i++){
+    for (var i = 0; i < numCols; i++){
 
       // place the rook by toggling the piece (0 -> 1)
-      board.togglePiece(startRow, i);
+      board.togglePiece(rowIndex, i);
 
       // check if there are any conflicts now that the piece has been placed
       // if we've reached the end of the board, we want to return the solution 
       // (before we start toggling the pieces back)
       if (!rookBoard.hasAnyRooksConflicts()){
-        var result = getRookSolution(rookBoard, startRow+1, numRows);
+        var result = getRookSolution(rookBoard, rowIndex+1, numCols);
         if (result) { return solution = result; }
       }
 
       // take the rook out of that place (1 -> 0)
-      board.togglePiece(startRow, i);
+      board.togglePiece(rowIndex, i);
     }
   };
 
-  getRookSolution(board, 0, n, []);
+  getRookSolution(board, 0, n);
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
@@ -56,18 +53,74 @@ window.findNRooksSolution = function(n){
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n){
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var board = new Board({n:n});
 
+  // recursive subroutine - with everything you keep track of passed as arguments
+  var countRookSolutions = function(rookBoard, rowIndex, numCols) {
+
+    // exit strategy - when we're in the last row and we've placed a rook
+    // want to increment the count
+    if (rowIndex === numCols) {
+      solutionCount++;
+      return;
+    }
+
+    // otherwise keep trying to place the rook
+    for (var i = 0; i < numCols; i++){
+
+      // place the rook by toggling the piece (0 -> 1)
+      board.togglePiece(rowIndex, i);
+
+      // check if there are any conflicts now that the piece has been placed
+      // if we've reached the end of the board, we want to return the solution 
+      // (before we start toggling the pieces back)
+      if (!rookBoard.hasAnyRooksConflicts()){
+        countRookSolutions(rookBoard, rowIndex+1, numCols);
+      }
+
+      // take the rook out of that place (1 -> 0)
+      board.togglePiece(rowIndex, i);
+    }
+  };
+
+  countRookSolutions(board, 0, n, []);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
 
-
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n){
-  var solution = undefined; //fixme
+  var solution;
+  var board = new Board({n:n});
 
+  var getQueenSolution = function(queenBoard, rowIndex, numCols) {
+    if (rowIndex === numCols) {
+      return queenBoard.rows();
+    }
+
+    // otherwise keep trying to place the rook
+    for (var i = 0; i < numCols; i++){
+
+      // place the rook by toggling the piece (0 -> 1)
+      board.togglePiece(rowIndex, i);
+
+      // check if there are any conflicts now that the piece has been placed
+      // if we've reached the end of the board, we want to return the solution 
+      // (before we start toggling the pieces back)
+      if (!queenBoard.hasAnyQueensConflicts()){
+        var result = getQueenSolution(queenBoard, rowIndex+1, numCols);
+        if (result) { return solution = result; }
+      }
+
+      // take the rook out of that place (1 -> 0)
+      board.togglePiece(rowIndex, i);
+    }
+
+  };
+
+  getQueenSolution(board, 0, n);
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
