141c62e04fc06b61a4f9696af8668803ef35dac8
jgpettibone <jeanette.pettibone@gmail.com>
2014-05-08 09:48:20 -0700

    Implemented bitwise solution to count nqueens

--- src/solvers.js
+++ src/solvers.js
@@ -49,8 +49,6 @@ window.findNRooksSolution = function(n){
   return solution;
 };
 
-
-
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 // window.countNRooksSolutions = function(n){
 //   var solutionCount = 0;
@@ -93,11 +91,18 @@ window.findNRooksSolution = function(n){
 // we know that we can place the first rook in one of 4 places
 // and the second rook in one of 3 places, etc 
 // so we can do this without using a board
-window.countNRooksSolutions = function(n){
-  if (n === 0) { return 1; }
-  return n * countNRooksSolutions(n-1);
-};
+// window.countNRooksSolutions = function(n){
+//   if (n === 0) { return 1; }
+//   return n * countNRooksSolutions(n-1);
+// };
 
+// Should always do factorial with iterative approach
+// faster and no call stack problems
+window.countNRooksSolution = function(n) {
+  var result = 1;
+  for (var i = n; n > 0; i--) result *= i;
+  return result;
+}
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n){
@@ -125,6 +130,54 @@ window.findNQueensSolution = function(n){
   return solution;
 };
 
+
+
+window.countNQueensSolutionsBitwise = function(n) {
+  var solutionCount = 0;
+
+  // uses minDiagConflict, majDiagConflict, colConflict for conflicts in the 
+  // minor diagonal, major diagonal, and column
+  // only placing one queen per row so not testing row conflicts
+  var countQueenSolutions = function(currentRow, minDiagConflict, majDiagConflict, colConflict) {
+
+    // if we've hit n (the index past the last row), then we've found a solution
+    if (currentRow === n) {
+      solutionCount++;
+      return;
+    }
+
+    // we have a conflict if we have any type of conflict    
+    var conflicts = minDiagConflict | majDiagConflict | colConflict;
+
+    // starts at 1 and then goes up through increments of 2 (binary numbers)
+    // until it shifts over to the size of the board 
+    // ex: if n = 4 , (1 << 4) = 10000 = 16, so the last iteration will be 
+    // when i = 8 = 1000 [1 = 0001, 2 = 0010, 4 = 0100, 8 = 1000]
+    // so i represents the position of the queen in the row
+    for (var i = 1; i < 1<<n; i*=2) {
+
+      // if the position of the queen is 1, we want it to AND with 
+      // a conflicts that has a 0 in that position so that 
+      // conflicts & i is 0
+      if (!(conflicts & i)) {
+        // the new minDiagConflict and majDiagConflicts are found by 
+        // ORing the current queen position to them and then shifting
+        // them in the correct direction
+        // for instance if the queen is in 1000 (8) and the current 
+        // minDiagConflict is in           0100 (4)
+        // then add the queen position     1100 (12)
+        // and shift it to the left       11000 (24)
+        var nextMinDiagConflict = (minDiagConflict | i) << 1;
+        var nextMajDiagConflict = (majDiagConflict | i) >> 1;
+        result = countQueenSolutions(currentRow+1, nextMinDiagConflict, nextMajDiagConflict, colConflict | i);
+        if (result) return solutionCount = result;
+      }
+    }
+  };
+
+  countQueenSolutions(0,0,0,0);
+  return solutionCount;
+};
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n){
